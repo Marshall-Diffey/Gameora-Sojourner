71546262e3496098099de3236c132bc67e1fc94f
Marshall-Diffey <marshalldiffey@yahoo.com>
2021-03-09 16:28:15 -0600

    Implemented questions pug files, get questions route, and require auth login message

--- routes/questions.js
+++ routes/questions.js
@@ -7,4 +7,23 @@ const { csrfProtection, asyncHandler } = require('./utils')
 const db = require('../db/models')
 const { validationResult, check } = require('express-validator')
 const registrationsValidations = require('./registerValidations')
-const { loginUser, logOutUser } = require('../auth')
+const { requireAuth } = require('../auth')
+
+
+router.get('/', csrfProtection, requireAuth, asyncHandler(async(req, res) => {
+    const topics = await db.Topic.findAll();
+    console.log('cheese');
+    
+    res.render('new-question', {
+        csrfToken: req.csrfToken(),
+        topics,
+        title: "New Question"
+    })
+}));
+
+// router.post('/',)
+// router.get('/:id',
+// router.delete('/:id',)
+
+
+module.exports = router;
