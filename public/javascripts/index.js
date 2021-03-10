ea9a8fe7796635132c7ed16ad5609f37aade46bd
baylen doss <baylendoss@baylens-Mini.lan>
2021-03-10 17:48:35 -0600

    heroku

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,18 @@
-window.addEventListener("load", (event)=>{
-    console.log("hello from javascript!")
-})
+
+
+const deleteButtons = document.querySelectorAll('.delete-button');
+const comment = document.getElementById('comment-div')
+deleteButtons.forEach((button) => button.addEventListener('click', async (event) => {
+ 
+    
+    const questionId = event.target.value
+    
+    const result = await fetch(`/questions/${questionId}`, {
+        method: 'DELETE'
+
+    })
+    comment.innerHTML = `You have deleted your question, redirecting to homepage...`
+    setTimeout(() => {
+        window.location.href='/'
+    }, 3000)
+}))
