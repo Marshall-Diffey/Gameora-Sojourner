5a46ec0d3f47a09c8248db8a09f2d653eff3f571
baylen doss <baylendoss@baylens-Mini.lan>
2021-03-11 13:18:32 -0600

    comment adding and delete functionality

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,48 @@
 
 
+let commentDeleteButtons = document.querySelectorAll('.commentDeleteButton');
 const deleteButtons = document.querySelectorAll('.delete-button');
-const comment = document.getElementById('comment-div')
+const comment = document.getElementById('comment-div');
+
+const addCommentButton = document.querySelector('.addCommentButton');
+const newComment = document.getElementById('newComment');
+const questionId = document.getElementsByName('questionId');
+const commentDivs = document.getElementsByName('comments');
+const questionsDiv = document.getElementById('questionDiv');
+//delete every comment breaks whole page
+addCommentButton.addEventListener('click', async (event) => {
+    event.preventDefault();
+
+
+    const body = {
+        newComment: newComment.value,
+        questionId: questionId[0].value
+    };
+
+    const result = await fetch(`/comments`, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+
+    })
+    newComment.value = ''
+    const res = await result.json();
+    const newCommentDiv = document.createElement('div');
+    newCommentDiv.setAttribute('name', 'comments');
+    newCommentDiv.setAttribute('id', 'comment');
+    newCommentDiv.innerText = res.comment.body;
+    questionDiv.appendChild(newCommentDiv);
+    const deleteButton = document.createElement('button');
+    deleteButton.setAttribute('class', 'commentDeleteButton');
+    deleteButton.value = res.comment.id;
+    deleteButton.innerText = 'Delete Comment';
+    newCommentDiv.appendChild(deleteButton);
+    commentDeleteButtons = document.querySelectorAll('.commentDeleteButton');
+    deleteListener()
+
+})
 deleteButtons.forEach((button) => button.addEventListener('click', async (event) => {
 
 
@@ -16,3 +57,18 @@ deleteButtons.forEach((button) => button.addEventListener('click', async (event)
         window.location.href = '/'
     }, 3000)
 }))
+
+const deleteListener = () => {
+    commentDeleteButtons.forEach((button) => button.addEventListener('click', async (event) => {
+        const commentId = event.target.value
+
+        event.target.parentElement.remove()
+        const result = await fetch(`/comments/${commentId}`, {
+            method: 'DELETE'
+        })
+
+
+
+    }))
+}
+deleteListener()
--- routes/comments.js
+++ routes/comments.js
@@ -7,19 +7,60 @@ const { requireAuth } = require('../auth')
 
 const commentValidations = [
     check('newComment')
-      .exists({ checkFalsy: true })
-      .withMessage('Please provide a value for the comment')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a value for the comment')
 ];
 
- router.post('/', csrfProtection, requireAuth, commentValidations, asyncHandler(async (req, res) => {
-    const {newComment} = req.body;
-    const { userId } = req.session.auth;
-    const questionId = parseInt(req.body.questionId, 10)
-    const question = await db.Question.findByPk(questionId);
-    const comments = await db.Comment.findAll({where: {questionId}});
+// router.post('/', csrfProtection, requireAuth, commentValidations, asyncHandler(async (req, res) => {
+//     const { newComment } = req.body;
+//     const { userId } = req.session.auth;
+//     const questionId = parseInt(req.body.questionId, 10)
+//     const question = await db.Question.findByPk(questionId);
+//     const comments = await db.Comment.findAll({ where: { questionId } });
 
+//     console.log(newComment);
+//     console.log(questionId);
+
+//     const comment = db.Comment.build({
+//         body: newComment,
+//         userId,
+//         questionId
+//     });
+//     const validatorErrors = validationResult(req);
+
+//     if (validatorErrors.isEmpty()) {
+
+//         await comment.save();
+//         return req.session.save(() => {
+//             res.redirect(`/questions/${questionId}`)
+//         })
+//     } else {
+//         const topics = await db.Topic.findAll();
+//         const errors = validatorErrors.array().map(error => error.msg)
+//         return res.render('question', {
+//             title: 'Comment Error',
+//             comments,
+//             question,
+//             csrfToken: req.csrfToken(),
+//             errors,
+//             userId
+//         })
+//     }
+// }));
+router.post('/', requireAuth, commentValidations, asyncHandler(async (req, res) => {
+    const { newComment, } = req.body;
     console.log(newComment);
-    console.log(questionId);
+    const { userId } = req.session.auth;
+    console.log('what is this??', userId);
+    const questionId = parseInt(req.body.questionId, 10)
+    // console.log('this is a string 123')
+    // console.log('this is not a string', 123)
+    console.log('what is this??', questionId);
+    const question = await db.Question.findByPk(questionId);
+    const comments = await db.Comment.findAll({ where: { questionId } });
+
+    //console.log(newComment);
+    //console.log(questionId);
 
     const comment = db.Comment.build({
         body: newComment,
@@ -28,36 +69,40 @@ const commentValidations = [
     });
     const validatorErrors = validationResult(req);
 
-      if (validatorErrors.isEmpty()) {
+    if (validatorErrors.isEmpty()) {
 
-          await comment.save();
-           return req.session.save(() => {
-            res.redirect(`/questions/${questionId}`)
-           })
-      } else {
-          const topics = await db.Topic.findAll();
-          const errors = validatorErrors.array().map(error => error.msg)
-          return res.render('question', {
-              title: 'Comment Error',
-              comments,
-              question,
-              csrfToken: req.csrfToken(),
-              errors,
-              userId
-          })
-      }
- }));
+        await comment.save();
+        res.json({ comment })
+        // return req.session.save(() => {
+        // })
+    } else {
+        const topics = await db.Topic.findAll();
+        const errors = validatorErrors.array().map(error => error.msg)
+        return res.render('question', {
+            title: 'Comment Error',
+            comments,
+            question,
+            csrfToken: req.csrfToken(),
+            errors,
+            userId
+        })
+    }
+}));
+
+
+
+
+
+
 // going to edit this route to delete comments, not questions
- router.delete('/:id(\\d+)', requireAuth, asyncHandler(async(req, res) => {
-    const questionId = parseInt(req.params.id, 10);
-    const question = await db.Question.findByPk(questionId);
-    if (question === null) {
+router.delete('/:id(\\d+)', requireAuth, asyncHandler(async (req, res) => {
+    const commentId = parseInt(req.params.id, 10);
+    const comment = await db.Comment.findByPk(commentId);
+    if (comment === null) {
         return res.redirect('/');
     }
-    return await question.destroy({
-        where: { id: questionId }
-    }).end()
- }))
+    return await comment.destroy({})
+}))
 
 // router.delete('/:id',)
 // router.put('/:id',) bonus route if we have time to implement question editing
