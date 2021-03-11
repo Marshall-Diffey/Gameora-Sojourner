eeca698cca5b6df291bab84dda23cf59ce2b5208
jeb griffin <jebgriffin@jebs-MacBook-Pro.local>
2021-03-11 10:10:22 -0600

    questions render in order

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,12 @@ const loginUser = require('../auth')
 
 /* GET home page. */
 router.get('/', asyncHandler(async (req, res, next) => {
-  const questions = await db.Question.findAll({}).map(question => question.dataValues)
+  const questions = await db.Question.findAll({
+    limit: 15,
+    order: [
+      ['id', 'DESC']
+    ]
+  }).map(question => question.dataValues)
   const topics = await db.Topic.findAll({}).map(topic => topic.dataValues)
 
 
