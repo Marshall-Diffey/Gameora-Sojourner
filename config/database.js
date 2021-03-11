93fa185a7487674b9bee9d5dcc7c3d532461f67a
baylen doss <baylendoss@baylens-Mini.lan>
2021-03-10 20:37:20 -0600

    heroku

--- config/database.js
+++ config/database.js
@@ -10,5 +10,9 @@ module.exports = {
     host,
     dialect: 'postgres',
     seederStorage: 'sequelize',
-  },
+  }, production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+  }
 };
