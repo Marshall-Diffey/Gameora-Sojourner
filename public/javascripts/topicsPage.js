bb5f109aa2e837f835a0ac5e47d457d714258b66
baylen doss <baylendoss@baylens-Mini.lan>
2021-03-12 14:42:43 -0600

    oops

---
+++ public/javascripts/topicsPage.js
@@ -0,0 +1,14 @@
+const homeDivs = document.getElementsByName('question')
+
+
+homeDivs.forEach(question => question.addEventListener('click', event => {
+
+    window.location.href = `/questions/${event.target.id}`
+}))
+
+const homeDivTitle = document.getElementsByName("question-container")
+homeDivTitle.forEach(questionz => questionz.addEventListener('click', event => {
+    const id = event.target.id
+    console.log(id)
+    window.location.href = `/questions/${id}`
+}))
