59e35d50a815900be21d2c8f525391eba5a80abe
baylend123 <75502053+baylend123@users.noreply.github.com>
2021-03-12 17:33:38 -0600

    fixed topics clicking again

--- public/javascripts/topicsPage.js
+++ public/javascripts/topicsPage.js
@@ -2,13 +2,17 @@ const homeDivs = document.getElementsByName('question')
 
 
 homeDivs.forEach(question => question.addEventListener('click', event => {
+    if (event.target.id) {
 
-    window.location.href = `/questions/${event.target.id}`
+
+        window.location.href = `/questions/${event.target.id}`
+    }
 }))
 
 const homeDivTitle = document.getElementsByName("question-container")
 homeDivTitle.forEach(questionz => questionz.addEventListener('click', event => {
-    const id = event.target.id
-    console.log(id)
-    window.location.href = `/questions/${id}`
+    if (event.target.id) {
+
+        window.location.href = `/questions/${event.target.id}`
+    }
 }))
